3c95e9726fcfd0432b2e94ffd6bccc3cddd66bfb
huydt84 <viethuy061002@gmail.com>
2024-08-13 14:54:52 +0700

    densenet/add model architecture

--- models/densenet/densenet.py
+++ models/densenet/densenet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class DenseLayer(nn.Module):
     def __init__(self, num_input_features, growth_rate, bn_size=4, drop_rate=0.0):
@@ -19,7 +20,8 @@ class DenseLayer(nn.Module):
             self.layer.add_module('dropout', nn.Dropout(p=drop_rate, inplace=True))
         
     def forward(self, x):
-        new_feature = self.layer(x)
+        concated_feature = torch.cat(x, 1)
+        new_feature = self.layer(concated_feature)
         return new_feature
     
 class DenseBlock(nn.ModuleDict):
@@ -54,3 +56,72 @@ class Transition(nn.Sequential):
     def forward(self, x):
         return self.down_sample(x)
 
+class DenseNet(nn.Module):
+    
+    # Densenet-BC model (default is densenet121)
+    def __init__(self, image_channels=3, growth_rate=32, block_config=(6, 12, 24, 16),
+                 num_init_features=64, bn_size=4, reduction=2, drop_rate=0, num_classes=100):
+
+        super().__init__()
+
+        # First convolution
+        self.features = nn.Sequential(
+            nn.Conv2d(image_channels, num_init_features, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(num_init_features),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        # Each dense block
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate
+            )
+            self.features.add_module('dense_block_%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = Transition(num_input_features=num_features,
+                                    num_output_features=num_features // reduction)
+                self.features.add_module('transition_%d' % (i + 1), trans)
+                num_features = num_features // reduction
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+    
+def densenet121(image_channels=3):
+    return DenseNet(image_channels, growth_rate=32, block_config=(6, 12, 24, 16), num_init_features=64)
+
+def densenet161(image_channels=3):
+    return DenseNet(image_channels, growth_rate=48, block_config=(6, 12, 36, 24), num_init_features=96)
+
+def densenet169(image_channels=3):
+    return DenseNet(image_channels, growth_rate=32, block_config=(6, 12, 32, 32), num_init_features=64)
+
+def densenet201(image_channels=3):
+    return DenseNet(image_channels, growth_rate=32, block_config=(6, 12, 48, 32), num_init_features=64)
