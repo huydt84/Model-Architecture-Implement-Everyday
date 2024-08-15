f68913952f4048598d43eb14517a77b781033035
huydt84 <viethuy061002@gmail.com>
2024-08-15 21:47:28 +0700

    vgg16/add weight initialization

--- models/vgg16/model.py
+++ models/vgg16/model.py
@@ -91,6 +91,19 @@ class VGG16(nn.Module):
         )
         self.output = nn.Linear(4096, self.num_classes)
         
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+        
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
