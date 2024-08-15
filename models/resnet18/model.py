20e677f28b4fe0db89139d95ae49dbbf48895680
huydt84 <viethuy061002@gmail.com>
2024-08-15 21:55:19 +0700

    resnet18/add input shape validation

--- models/resnet18/model.py
+++ models/resnet18/model.py
@@ -68,7 +68,13 @@ class ResNet18(nn.Module):
             ResidualBlock(out_channels, out_channels, mid_channels=self.mid_channels)
         )
         
-    def forward(self, x):     
+    def forward(self, x): 
+        if len(x.shape) == 3:
+            x = x.unsqueeze(0)
+        if len(x.shape) != 4:
+            raise ValueError("Shape of input must be (batch, channel, height, width) or (channel, height, width). "
+                             f"Your input shape currently is {x.shape}")  
+              
         x = self.layer(x)
         
         x = self.res1(x)
